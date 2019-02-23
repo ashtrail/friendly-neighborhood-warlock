e269d90a2566e5bfddb88fa69db637ca00a62e3e
Arthur Cendrier <arthur.cendrier@epitech.eu>
2019-02-23 17:23:40 +0100

    Add cancel and submit button

--- entities/elements/Element.gd
+++ entities/elements/Element.gd
@@ -8,3 +8,4 @@ func _on_Area2D_input_event(_viewport, event, _shape_idx):
 	if (event is InputEventMouseButton && event.pressed):
 		print('clique')
 		emit_signal("spell_cast", type)
+		$AnimationPlayer.play("clicked")
