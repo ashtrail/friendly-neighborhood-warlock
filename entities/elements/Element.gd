b63e71adfea508ed2140fd41cd6903f23100a447
Arthur Cendrier <arthur.cendrier@epitech.eu>
2019-02-24 10:59:22 +0100

    Add idle animation to elements

--- entities/elements/Element.gd
+++ entities/elements/Element.gd
@@ -5,6 +5,10 @@ export (Array) var sfx
 
 signal spell_cast(type)
 
+func _ready():
+	$AnimationPlayer.play("idle")
+
+
 func get_sfx() -> AudioStreamSample:
 	var index = randi() % Global.SPELL_SFX[type].size()
 	return Global.SPELL_SFX[type][index]
@@ -16,3 +20,7 @@ func _on_Area2D_input_event(_viewport, event, _shape_idx):
 		$AnimationPlayer.play("clicked")
 		$AudioStreamPlayer.stream = get_sfx()
 		$AudioStreamPlayer.play()
+
+
+func _on_AudioStreamPlayer_finished():
+	$AnimationPlayer.play("idle")
